684de31cbf5087dd929d97cf904ee0758f04dd9d
MorelessCSS <rf@appon.net>
2017-07-30 09:15:25 +0200

    initial

--- metrics.php
+++ metrics.php
@@ -17,9 +17,9 @@ echo "instanceID:$instanceId\n";
 $pushMetrics = array();
 foreach ($config -> metrics as $metrics) {
     foreach ($metrics as $metricName => $metric) {
-       $className = "CWScripts\\plugins\\" . $metric->$name;
-       if (var_dump(class_exists($metric->$name))){
-            $ref = $metric->$name;
+       $className = "CWScripts\\plugins\\" . $metricName;
+       if (var_dump(class_exists($metricName))){
+            $ref = $metricName;
             $obj = new $ref();
             }
        else {
